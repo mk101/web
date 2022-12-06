7d160a72c78c0a45c538c49330e8ed580736b770
Maxim Kolesov <pro100maks222@gmail.com>
2022-12-06 15:23:53 +0300

    Fix router

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,4 +1,4 @@
-const Router = require("express");
+const Router = require("express").Router;
 const router = new Router()
 const authController = require('../controllers/auth.controller')
 
--- routes/frontend.route.js
+++ routes/frontend.route.js
@@ -1,4 +1,4 @@
-const Router = require("express");
+const Router = require("express").Router;
 const router = new Router()
 const frontendController = require("../controllers/frontend.controller");
 
